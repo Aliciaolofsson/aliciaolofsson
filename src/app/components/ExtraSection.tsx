bee6deb45cb756dba052b66941ecead0b97e0ca9
Aliciaolofsson <Alicia-olofsson@hotmail.com>
2023-09-13 23:21:17 +0200

    changed padding and margin

--- src/app/components/ExtraSection.tsx
+++ src/app/components/ExtraSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ExtraSection() {
   return (
     <div>
-      <section className='flex justify-center h-5/6 bg-black p-60 my-10 '>
+      <section className='flex justify-center h-5/6 bg-black py-60 my-10 '>
         <p className='text-white text-lg font-semibold tracking-wider'>I&apos;m into design and code</p>
       </section>
     </div>
